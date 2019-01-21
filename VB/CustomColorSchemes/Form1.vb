9c1c62afb38e76d36c6c07d03c72989d9f41f021
DevExpressExampleBot <fakeEmail>
2019-01-21 14:37:25 +0300

    Source auto update [skip ci]

---
+++ VB/CustomColorSchemes/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports DevExpress.XtraReports.Wizards
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows.Forms
+
+Namespace CustomColorSchemes
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+
+            'Register the wizard customization service.
+            reportDesigner1.AddService(GetType(IWizardCustomizationService), New WizardCustomizationService())
+        End Sub
+    End Class
+End Namespace
